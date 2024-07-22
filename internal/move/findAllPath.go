5f3ea0de803a2fe712d77a631823bd50474cda14
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-07-22 17:03:50 +0000

    .

--- internal/move/findAllPath.go
+++ internal/move/findAllPath.go
@@ -3,54 +3,53 @@ package move
 import (
 	"fmt"
 	data "main/internal/data"
-	 "math/rand"
-	 "slices"
+	"math/rand"
+	"slices"
 )
 
-type ant struct {
-	WhereAmI string
-}
+// type ant struct {
+// 	WhereAmI string
+// }
 
 func FindAllPaths(myGraph *data.Graph) {
 
-	//var ants map[int]ant
-	ants := make(map[int]ant)
 
-	var path []string
-	path = append(path, myGraph.StartRoomName)
+	var path  []string
 
-	ants[1] = ant{WhereAmI: myGraph.StartRoomName}
-	//whereIsTheNextAvailableTunnel
-	//numberOfRooms := len(myGraph.Rooms)
-	currentRoomName := myGraph.StartRoomName
-	numberOfTry :=0
+	var allPath [][]string
 
+	for i := 0; i < 20; i++ {
+		
+		path = nil
+		path = append(path, myGraph.StartRoomName)
+		currentRoomName := myGraph.StartRoomName
+		numberOfTry := 0
 
-for {
-	numberOfTry ++
-	if currentRoomName == myGraph.EndRoomName || numberOfTry >10000 {break}
+		for {
+			numberOfTry++
+			if currentRoomName == myGraph.EndRoomName || numberOfTry > 10000 {
+				break
+			}
 
-	currentRoom := myGraph.Rooms[currentRoomName]
-	currentTunnerArr := currentRoom.Tunnels
-	lengthCurrentTunnerArr := len(currentTunnerArr)
+			currentRoom := myGraph.Rooms[currentRoomName]
+			currentTunnerArr := currentRoom.Tunnels
+			lengthCurrentTunnerArr := len(currentTunnerArr)
 
+			randomNextRoomIndex := rand.Intn(lengthCurrentTunnerArr)
+			nextRandomRoomName := currentTunnerArr[randomNextRoomIndex]
 
+			if slices.Contains(path, nextRandomRoomName) {
+				continue
+			}
 
-	randomNextRoomIndex := rand.Intn(lengthCurrentTunnerArr)
-	nextRandomRoomName := currentTunnerArr[randomNextRoomIndex]
+			currentRoomName = nextRandomRoomName
+			path = append(path, currentRoomName)
 
-	if slices.Contains(path,nextRandomRoomName) {continue}
-	
-	currentRoomName = nextRandomRoomName
-	path = append(path, currentRoomName)
-	
-}
+		}
+		fmt.Println(path)
+		allPath = append(allPath,path)
+	}
+	fmt.Println(allPath)
 
-
-	fmt.Println(path)
-	//fmt.Println(currentRoom)
-	//fmt.Println("Finding")
-	//fmt.Println(myGraph)
-	//fmt.Println(rand.Intn(5)) //0-4
 
 }
