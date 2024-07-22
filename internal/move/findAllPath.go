d372ea9d851ba660fa1fa76a24aaf77626a0fd8f
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-07-22 17:21:12 +0000

    0

--- internal/move/findAllPath.go
+++ internal/move/findAllPath.go
@@ -16,12 +16,17 @@ func FindAllPaths(myGraph *data.Graph) {
 	var path []string
 
 	var allPath [][]string
+	var pathStr string
+
+	var allPathStr []string
 
 	for i := 0; i < 20; i++ {
-
+		pathStr = ""
 		path = nil
 		path = append(path, myGraph.StartRoomName)
 		currentRoomName := myGraph.StartRoomName
+		pathStr = currentRoomName
+
 		numberOfTry := 0
 
 		for {
@@ -43,11 +48,32 @@ func FindAllPaths(myGraph *data.Graph) {
 
 			currentRoomName = nextRandomRoomName
 			path = append(path, currentRoomName)
+			pathStr += "-" + currentRoomName
 
 		}
 		fmt.Println(path)
+		if path[len(path)-1]!= myGraph.EndRoomName{
+			continue
+		}
+		//fmt.Println(pathStr)
+		if slices.Contains(allPathStr, pathStr){
+			continue
+		}
+
+
+		allPathStr = append(allPathStr, pathStr)
 		allPath = append(allPath, path)
 	}
-	fmt.Println(allPath)
+	fmt.Println("-------All paths:-----------")
+	for _, path := range allPath {
+		fmt.Println(path)
+		//fmt.Println(allPathStr)
+	}
+	for _, pathStr := range allPathStr {
+		fmt.Println(pathStr)
+	}
+	//----------------------------------------------------------------
+	//var allPathNo [][]string
 
+	// --------------------------------
 }
