6ce9f5c4c003e267ead3d3550eefffc7cff234ca
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-07-22 14:43:24 +0000

    .

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	util "main/internal/data"
+	data "main/internal/data"
 )
 
 func main() {
 	//var tunnelArr []string
 
-	var myGraph util.Graph
+	var myGraph data.Graph
 	tunnelArr := []string{"0-4", "0-6", "1-3", "4-3","5-2","3-5","4-2","2-1","7-6","7-2","7-4","6-5"}
 	myGraph.InitGraph(tunnelArr, 1, 0)
 	myGraph.PrintGraph()
