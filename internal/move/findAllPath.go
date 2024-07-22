453e2d01ddec46499deb4ae76fc4d327baa82697
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-07-22 18:56:03 +0000

    .

--- internal/move/findAllPath.go
+++ internal/move/findAllPath.go
@@ -57,34 +57,62 @@ func FindAllPaths(myGraph *data.Graph) {
 
 		}
 		//fmt.Println(path)
-		if path[len(path)-1]!= myGraph.EndRoomName{
+		if path[len(path)-1] != myGraph.EndRoomName {
 			continue
 		}
 		//fmt.Println(pathStr)
-		if slices.Contains(allPathStr, pathStr){
+		if slices.Contains(allPathStr, pathStr) {
 			continue
 		}
-
 
 		allPathStr = append(allPathStr, pathStr)
 		allPath = append(allPath, path)
 	}
 	fmt.Println("-------All paths:-----------")
-	fmt.Println(len(allPath))
-	//allPathMap := make(map[int][]string)
+	//fmt.Println(len(allPath))
+	allPathMap := make(map[int][][]string)
+	//allPathMap
+	// mio := [][]string{
+	//     {"path1-step1", "path1-step2", "path1-step3"},
+	//     {"path1-alt-step1", "path1-alt-step2"},
+	// }
+
+	//var mio [][]string
+	//mio=append(mio, path)
+	//allPathMap[4] = mio
+
+	//allPathMap[10] = []string{"a-b", "a-c", "reza-3", "4-3"}
 
 	for _, path := range allPath {
-		//allPathMap[len(path)]=path
-		fmt.Println(path,"=",len(path))
+		//mio=append(mio, path)
+		//allPathMap[len(path)]=mio
+		allPathMap[len(path)] = append(allPathMap[len(path)], path)
+
+		//fmt.Println(path, "=", len(path))
 		//fmt.Println(allPathStr)
 	}
-	fmt.Println(len(allPathStr))
-	for _, pathStr := range allPathStr {
-		fmt.Println(pathStr)
-	}
+	fmt.Println("number of paths:>>",len(allPathStr))
+	fmt.Println()
+	// for _, pathStr := range allPathStr {
+	// 	fmt.Println(pathStr)
+	// }
 	//----------------------------------------------------------------
 	//var allPathNo [][]string
 	//fmt.Println(allPathMap)
+	// for length, p:= range allPathMap {
+	// 	fmt.Printf("length: %d, paths: %v\n", length, p)
+	// 	fmt.Println()
+	// 	//fmt.Println(value)
+	// 	//allPathNo=append(allPathNo, value)
+	// }
 
+
+	for i := 0; i < 100; i++ {
+		
+
+		if allPathMap[i]==nil {continue}
+
+		fmt.Printf("length: %d, paths: %v\n", i, allPathMap[i])
+	}
 	// --------------------------------
 }
