c8dfd007a58b3f1a5aef2978db9a7666b052f398
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-07-22 13:57:06 +0000

    .

--- util/initGraph.go
+++ util/initGraph.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 )
 
-func (myGraph Graph)InitGraph(tunnelArr []string) error {
+func (myGraph Graph)InitGraph(tunnelArr []string,start, end uint) error {
 
-	myGraph.startRoomName = 0
-	myGraph.endRoomName = 6
+	myGraph.startRoomName = start
+	myGraph.endRoomName = end
 
 	myGraph.rooms = make(map[uint]Room)
-	
+
 	err := myGraph.setTunnelToGraph(tunnelArr)
 	if err != nil {
 		fmt.Println("Error:", err)
