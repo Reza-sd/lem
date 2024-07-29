2df7dbb4d5bbb0eac9d9652dc2e802a0e64ed5aa
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-07-29 15:34:55 +0000

    .

--- internal/move/move.go
+++ internal/move/move.go
@@ -12,37 +12,14 @@ func (myAnts *Ants) Move(myGraph *data.Graph) {
 
 	for _, ant := range myAnts.Ants {
 		fmt.Println("Ant:", ant.Name, "Current Room:", ant.CurrentRoomName)
-		//fmt.Println("Shortest Path:", ant.ShortestPath)
+		
 	}
 	//------------------------------
 
-	// what are the next free rooms in the graph
 	currentRoomName := myAnts.Ants[1].CurrentRoomName
 	fmt.Println(currentRoomName)
 
-	// nextRooms := myGraph.GetFreeRooms(currentRoom)
-	// for w := 0; w < 100; w++ {
-	// 	if myGraph.Paths[w] != nil {
-	// 		fmt.Println("length: ", w, "paths: ", myGraph.Paths[w])
-	// 		break
-	// 	}
-	// }
-	/*
-	   currentroom
-	   nextrooms?
-	   if nextroomsfree?
-	   nextfreerooms?
-	   currentroom-nextfreeroom in whitch path?
-	   path with lower weight will select
-	*/
-	// whats the random next room
-	// currentRoom := myGraph.Rooms[currentRoomName]
-	// currentTunnerArr := currentRoom.Tunnels
-	// lengthCurrentTunnerArr := len(currentTunnerArr)
 
-	// randomNextRoomIndex := rand.Intn(lengthCurrentTunnerArr)
-	// nextRandomRoomName := currentTunnerArr[randomNextRoomIndex]
-	// fmt.Println("nextRandomRoomName=", nextRandomRoomName)
 	myAnts.ChooseTheBestPath(myGraph, currentRoomName)
 
 	// ------------------------------
@@ -59,10 +36,10 @@ func (myAnts *Ants) ChooseTheBestPath(myGraph *data.Graph, currentRoomName strin
 	}
 	//-----------------
 	// -----simulate not emtpy--------
-	roomTest := myGraph.Rooms["6"]
-	roomTest.EmptySeats = 0
-	myGraph.Rooms["6"] = roomTest
-	fmt.Println(">>>>", myGraph.Rooms["6"].EmptySeats)
+	// roomTest := myGraph.Rooms["6"]
+	// roomTest.EmptySeats = 0
+	// myGraph.Rooms["6"] = roomTest
+	// fmt.Println(">>>>", myGraph.Rooms["6"].EmptySeats)
 	//-------test----
 	//myGraph.StartRoomName = "7"
 	// Ant1 := myAnts.Ants[1]
