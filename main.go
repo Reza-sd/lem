a3421db5395c397b8ddfe838521440fe357290be
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-07-30 20:19:11 +0000

    .

--- main.go
+++ main.go
@@ -6,14 +6,38 @@ import (
 	graphpk "main/internal/graphpk"
 )
 
+
+type Lem struct{
+	NumberOfAnts int
+	StartRoom string
+	EndRoom string
+	TunnelArr []string
+
+}
+
 func main() {
 
+
+	lem1 := Lem {
+		NumberOfAnts: 3,
+		StartRoom: "1",
+		EndRoom: "0",
+		TunnelArr : []string{"0-4", "0-6", "1-3", "4-3", "5-2", "3-5", "4-2", "2-1", "7-6", "7-2", "7-4", "6-5"},
+	}
+
+
+	NumberOfAnts := lem1.NumberOfAnts
+	StartRoom :=lem1.StartRoom
+	EndRoom :=lem1.EndRoom
+
+	
+	tunnelArr := lem1.TunnelArr
+
 	var baseGraph graphpk.Graph
-	tunnelArr := []string{"0-4", "0-6", "1-3", "4-3", "5-2", "3-5", "4-2", "2-1", "7-6", "7-2", "7-4", "6-5"}
-	baseGraph.InitGraph(tunnelArr, "1", "0")
+	baseGraph.InitGraph(tunnelArr, StartRoom, EndRoom)
 
 	var baseAnts antpk.Ants
-	baseAnts.AntsInit(3, &baseGraph)
+	baseAnts.AntsInit(NumberOfAnts, &baseGraph)
 
 
 	var myTravelPlan antpk.TravelPlan
