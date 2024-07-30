6176af32564323d2a2afc30844325b45bd00b3da
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-07-30 23:34:03 +0000

    .

--- main.go
+++ main.go
@@ -2,21 +2,37 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"main/internal/simulationpk"
 )
 
 func main() {
 
 	lem1 := simulationpk.Lem{
-		NumberOfAnts: 3,
+		NumberOfAnts: 20,
 		StartRoom:    "1",
 		EndRoom:      "0",
 		TunnelArr:    []string{"0-4", "0-6", "1-3", "4-3", "5-2", "3-5", "4-2", "2-1", "7-6", "7-2", "7-4", "6-5"},
 	}
+
+	// lem2 := simulationpk.Lem{
+	// 	NumberOfAnts: 5,
+	// 	StartRoom:    "0",
+	// 	EndRoom:      "1",
+	// 	TunnelArr:    []string{"0-2","2-3","3-1"},
+	// }
+
+/*
+"0-2","2-3","3-1"
+*/
+	startTime:=time.Now()//
+	//lem1=lem2
 	model1 := lem1.ModelInit()
-	myTravelPlan := model1.Run(200)
+	myTravelPlan := model1.Run(2000)
 	fmt.Println("")
 	fmt.Println("Steps: ", myTravelPlan.Steps)
 	myTravelPlan.TheBestPlan.PrintAllAnts()
-
+	fmt.Println("time=",time.Since(startTime).Seconds(),"Seconds")
+	//fmt.Println("time=",time.Since(startTime).Minutes(),"Minutes")
+	//fmt.Println("time=",time.Since(startTime).Abs().Hours(),"Abs Hours")
 }
