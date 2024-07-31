749b89d20395314d0a89b426a874d57b30592698
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-07-31 12:39:14 +0000

    .

--- internal/simulationpk/RandomSimulator.go
+++ internal/simulationpk/RandomSimulator.go
@@ -2,18 +2,19 @@ package simulationpk
 
 import (
 	//"fmt"
-	//"fmt"
+	"fmt"
 	"main/internal/antpk"
-	//graphpk "main/internal/graphpk"
+	graphpk "main/internal/graphpk"
 )
 
+// ====================================================================
 func RandomSimulator(theModel *Model) (theBestFoundTravelPlan antpk.TravelPlan) {
 
 	var myTravelPlan antpk.TravelPlan
 	// runRandomSimulator(theModel *Model,NumberOfTry int)
 	for {
 
-		myTravelPlan = runRandomSimulator(theModel, 500)
+		myTravelPlan = runRandomSimulator(theModel, 200)
 		//myTravelPlan = simulationpk.RandomSimulator(&model1)
 
 		if myTravelPlan.Steps != 0 {
@@ -23,3 +24,31 @@ func RandomSimulator(theModel *Model) (theBestFoundTravelPlan antpk.TravelPlan) 
 
 	return myTravelPlan
 }
+
+// ====================================================================
+func runRandomSimulator(theModel *Model, NumberOfTry int) (finalTravelPlan antpk.TravelPlan) {
+
+	baseGraph := theModel.baseGraph
+	baseAnts := theModel.baseAnts
+	//maxTry := 200 //?
+	maxTryToPushAllAntsToEnd := theModel.baseAnts.AntsNumber * theModel.baseGraph.NumberOfAllRoom * 10
+
+	var tempTravelPlan antpk.TravelPlan
+	var tempGraph graphpk.Graph
+	var tempAnts antpk.Ants
+
+	for i := 1; i <= NumberOfTry; i++ {
+
+		graphpk.GraphCopy(baseGraph, &tempGraph)
+		antpk.AntsCopy(baseAnts, &tempAnts)
+
+		tempAnts.TryPushAllAntsToEnd(&tempGraph, &tempTravelPlan, maxTryToPushAllAntsToEnd)
+
+	}
+	// ------------------
+	fmt.Println("maxTryToPushAllAntsToEnd=", maxTryToPushAllAntsToEnd)
+	//--------------------
+	return tempTravelPlan
+}
+
+//===============================================================================
