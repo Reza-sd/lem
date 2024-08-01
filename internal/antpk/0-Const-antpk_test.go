9df6b072b34012899b1e57ff28b79b8ed87f94fd
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-08-01 08:56:52 +0000

    .

--- internal/antpk/0-Const-antpk_test.go
+++ internal/antpk/0-Const-antpk_test.go
@@ -1,15 +1,14 @@
 package antpk
 
-
 import (
-"testing"
 	"reflect"
+	"testing"
 )
 
 // ===============================================
 /*
 
-*/
+ */
 
 // ===============================================
 func TestHello(t *testing.T) {
@@ -17,31 +16,43 @@ func TestHello(t *testing.T) {
 	t.Run("1-an example of Ant struct type", func(t *testing.T) {
 
 		//---Arrange---
-		ant1:=Ant{
-			Name: "Reza",
+		ant1 := Ant{
+			Name:            "reza",
 			CurrentRoomName: "room_1",
-			VisitedRoomsArr: []string{"room_0","room_1"},
-			StepNumber: 2,
+			VisitedRoomsArr: []string{"room_0", "room_1"},
+			StepNumber:      2,
 		}
 		//---Act---
 
 		//---Assert----
-		assertIfStruct(t,ant1)
+		assertIfStruct(t, ant1)
 	})
 	//------------
-	t.Run("2-an example of Ants struct type", func(t *testing.T) {
+	t.Run("2-an example of AntGroup struct type", func(t *testing.T) {
 		//t.Skip()
 		//---Arrange---
-		ant1:=Ant{
-			Name: "Reza",
-			CurrentRoomName: "room_1",
-			VisitedRoomsArr: []string{"room_0","room_1"},
-			StepNumber: 2,
+		// ant1 := Ant{
+		// 	Name:            "reza",
+		// 	CurrentRoomName: "room_1",
+		// 	VisitedRoomsArr: []string{"room_0", "room_1"},
+		// 	StepNumber:      2,
+		// }
+
+		AntGroup1 := AntGroup{
+			NumberOfAnts: 1,
+			AntsMap: map[int]Ant{
+				1: {Name: "Ant1",
+					CurrentRoomName: "room_1",
+					VisitedRoomsArr: []string{"room_0", "room_1"},
+					StepNumber:      2,
+				},
+			},
+			NumberOfSequence: 2,
 		}
 		//---Act---
 
 		//---Assert----
-		assertIfStruct(t,ant1)
+		assertIfStruct(t, AntGroup1)
 	})
 	// ------------
 }
