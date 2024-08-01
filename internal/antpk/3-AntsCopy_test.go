3e3e5037ab86408ff0bfb777761a9a56f5a7c80b
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-08-01 22:43:52 +0000

    .

--- internal/antpk/3-AntsCopy_test.go
+++ internal/antpk/3-AntsCopy_test.go
@@ -33,7 +33,8 @@ func Test_AntsCopy(t *testing.T) {
 	t.Run(`3-return n same copy of baseAntGroup one item`, func(t *testing.T) {
 		//---Arrange---
 		baseAntGroup := Sample_AntGroup_1ant_initmode_room_0
-		secondAntGroup := AntGroup{
+		var secondAntGroup AntGroup
+		compareAntGroup := AntGroup{
 			NumberOfAnts: 1,
 			AntsMap: map[int]Ant{
 				1: {Name: "L1",
@@ -44,16 +45,41 @@ func Test_AntsCopy(t *testing.T) {
 			},
 			NumberOfSequence: 0,
 		}
+		ifSame := true
 		//---Act---
 		AntGroupCopy(baseAntGroup, &secondAntGroup)
 		//---Assert----
-		assert_If_Two_AntGroup_SameCopy(t, baseAntGroup, secondAntGroup)
+		assert_If_Two_AntGroup_SameCopy(t, compareAntGroup, secondAntGroup, ifSame)
 	})
 	//------------------
-	t.Run(`4-return n same copy of baseAntGroup two item`, func(t *testing.T) {
+	t.Run(`4-return not same copy of baseAntGroup one item`, func(t *testing.T) {
 		//---Arrange---
+		baseAntGroup := Sample_AntGroup_1ant_initmode_room_0
+		var secondAntGroup AntGroup
+		compareAntGroup := AntGroup{
+			NumberOfAnts: 999999,
+			AntsMap: map[int]Ant{
+				1: {Name: "L1",
+					CurrentRoomName: "room_0",
+					VisitedRoomsArr: []string{"room_0"},
+					StepNumber:      0,
+				},
+			},
+			NumberOfSequence: 0,
+		}
+		ifSame := false //not same
+		//---Act---
+		AntGroupCopy(baseAntGroup, &secondAntGroup)
+		//---Assert----
+		assert_If_Two_AntGroup_SameCopy(t, compareAntGroup, secondAntGroup, ifSame)
+	})
+	//------------------
+	t.Run(`5-return n same copy of baseAntGroup two item`, func(t *testing.T) {
+		//---Arrange---
+		var secondAntGroup AntGroup
+
 		baseAntGroup := sample_AntGroup_2ants_initmode_room_0
-		secondAntGroup := AntGroup{
+		compareAntGroup := AntGroup{
 			NumberOfAnts:     2,
 			NumberOfSequence: 0,
 			AntsMap: map[int]Ant{
@@ -69,19 +95,20 @@ func Test_AntsCopy(t *testing.T) {
 				},
 			},
 		}
+		ifSame := true
 		//---Act---
 		AntGroupCopy(baseAntGroup, &secondAntGroup)
 		//---Assert----
-		assert_If_Two_AntGroup_SameCopy(t, baseAntGroup, secondAntGroup)
+		assert_If_Two_AntGroup_SameCopy(t, compareAntGroup, secondAntGroup, ifSame)
 	})
 	//--------------------------
 }
 
 // ================================================
-func assert_If_Two_AntGroup_SameCopy(t testing.TB, antGroup1 AntGroup, antGroup2 AntGroup) {
+func assert_If_Two_AntGroup_SameCopy(t testing.TB, antGroup1 AntGroup, antGroup2 AntGroup, ifSame bool) {
 	t.Helper()
 
-	if !reflect.DeepEqual(antGroup1, antGroup2) {
+	if !reflect.DeepEqual(antGroup1, antGroup2) && ifSame {
 		t.Errorf("function returned error ")
 	}
 
