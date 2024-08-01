44920c9c871a21fe912c002f451890055dfe1f1a
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-08-01 11:47:21 +0000

    .

--- internal/antpk/0-Const-antpk_Samples.go
+++ internal/antpk/0-Const-antpk_Samples.go
@@ -1,16 +1,6 @@
 package antpk
 
-import (
-	"reflect"
-	"testing"
-)
-
-// ===============================================
-/*
-
- */
-
-// ===============================================
+// ==================Samples=============================
 var (
 	//-------Ant type example-----------
 	ant1 = Ant{
@@ -73,56 +63,15 @@ var (
 	//------------------
 )
 
-// ==================================
-func Test_Const_antpk(t *testing.T) {
-	//------------
-	t.Run("0-an example of non-struct type", func(t *testing.T) {
-		//---Arrange---
-		expectation := false
-		//---Act---
-		//---Assert----
-		//assertIfStruct(t, "miio") fail test
-		assertIfStruct(t, "mio", expectation)
-	})
-	//------------
-	t.Run("1-an example of Ant struct type", func(t *testing.T) {
-		//---Arrange---
-		expectation := true
-		//---Act---
-		//---Assert----
-		//assertIfStruct(t, "miio") fail test
-		assertIfStruct(t, travelPlan1, expectation)
-	})
-	//------------
-	t.Run("2-an example of AntGroup struct type", func(t *testing.T) {
-		//---Arrange---
-		expectation := true
-		//---Act---
-		//---Assert----
-		assertIfStruct(t, travelPlan1, expectation)
-	})
-	// ------------
-
-	t.Run("3-an example of TravelPlan struct type", func(t *testing.T) {
-		//---Arrange---
-		expectation := true
-		//---Act---
-		//---Assert----
-
-		assertIfStruct(t, travelPlan1, expectation)
-	})
-	//------------
-}
+// ===============================================
 
 // -----------------------------------------------------
-func assertIfStruct(t testing.TB, s interface{}, expectation bool) {
-	t.Helper()
+// func assertIfStruct(t testing.TB, s interface{}, expectation bool) {
+// 	t.Helper()
 
-	v := reflect.ValueOf(s)
-	if v.Kind() != reflect.Struct && expectation {
-		t.Errorf("not struct")
-	}
+// 	v := reflect.ValueOf(s)
+// 	if v.Kind() != reflect.Struct && expectation {
+// 		t.Errorf("not struct")
+// 	}
 
-}
-
-// ===============================================
+// }
