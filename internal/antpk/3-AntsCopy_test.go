4acf0a52681b820b09adceb315090cd59b9b379c
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-08-01 14:59:49 +0000

    .

--- internal/antpk/3-AntsCopy_test.go
+++ internal/antpk/3-AntsCopy_test.go
@@ -6,21 +6,14 @@ import (
 )
 
 func Test_AntsCopy(t *testing.T) {
-
 	//------------
 	t.Run(`0-return an error if baseAntGroup does not have any item`, func(t *testing.T) {
 		//---Arrange---
-		// var myAntGroup AntGroup
-		// numberOfAnts := 0
-		// startRoomName := "L1"
-		// expectError := true
-		//baseAntGroup := Sample_AntGroup_1ant_initmode_room_0
 		var secondAntGroup AntGroup
 		var baseAntGroup AntGroup
 		expectError := true
 		//---Act---
 		err := AntsCopy(baseAntGroup, &secondAntGroup)
-		//err := AntsCopy(Sample_AntGroup_1ant_initmode_room_0.AntsMap, secondAntGroup)
 		//---Assert----
 		assert_If_AntsCopy_ReturnError(t, err, expectError)
 
