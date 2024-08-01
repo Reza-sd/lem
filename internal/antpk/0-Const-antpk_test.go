926304ec13f51b010aa6a6b1e5a480b7a06f3024
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-08-01 09:10:09 +0000

    .

--- internal/antpk/0-Const-antpk_test.go
+++ internal/antpk/0-Const-antpk_test.go
@@ -11,46 +11,50 @@ import (
  */
 
 // ===============================================
+var(
+	//-------Ant type example-----------
+	ant1 = Ant{
+		Name:            "reza",
+		CurrentRoomName: "room_1",
+		VisitedRoomsArr: []string{"room_0", "room_1"},
+		StepNumber:      2,
+	}
+	//--------AntGroup type example---------
+	AntGroup1 = AntGroup{
+		NumberOfAnts: 1,
+		AntsMap: map[int]Ant{
+			1: {Name: "Ant1",
+				CurrentRoomName: "room_1",
+				VisitedRoomsArr: []string{"room_0", "room_1"},
+				StepNumber:      2,
+			},
+		},
+		NumberOfSequence: 2,
+	}
+	//-------AntGroup type example------
+	AntGroup2 = AntGroup{
+		NumberOfAnts: 1,
+		AntsMap: map[int]Ant{
+			1: ant1,
+		},
+		NumberOfSequence: 2,
+	}
+	//----------
+)
+
+//---------------------------------------------
 func TestHello(t *testing.T) {
 	//------------
 	t.Run("1-an example of Ant struct type", func(t *testing.T) {
-
 		//---Arrange---
-		ant1 := Ant{
-			Name:            "reza",
-			CurrentRoomName: "room_1",
-			VisitedRoomsArr: []string{"room_0", "room_1"},
-			StepNumber:      2,
-		}
 		//---Act---
-
 		//---Assert----
 		assertIfStruct(t, ant1)
 	})
 	//------------
 	t.Run("2-an example of AntGroup struct type", func(t *testing.T) {
-		//t.Skip()
 		//---Arrange---
-		// ant1 := Ant{
-		// 	Name:            "reza",
-		// 	CurrentRoomName: "room_1",
-		// 	VisitedRoomsArr: []string{"room_0", "room_1"},
-		// 	StepNumber:      2,
-		// }
-
-		AntGroup1 := AntGroup{
-			NumberOfAnts: 1,
-			AntsMap: map[int]Ant{
-				1: {Name: "Ant1",
-					CurrentRoomName: "room_1",
-					VisitedRoomsArr: []string{"room_0", "room_1"},
-					StepNumber:      2,
-				},
-			},
-			NumberOfSequence: 2,
-		}
 		//---Act---
-
 		//---Assert----
 		assertIfStruct(t, AntGroup1)
 	})
@@ -60,11 +64,8 @@ func TestHello(t *testing.T) {
 // -----------------------------------------------------
 func assertIfStruct(t testing.TB, s interface{}) {
 	t.Helper()
-	// if got != want {
-	// 	t.Errorf("got %q want %q", got, want)
-	// }
+	
 	v := reflect.ValueOf(s)
-
 	if v.Kind() != reflect.Struct {
 		t.Errorf("not struct")
 	}
