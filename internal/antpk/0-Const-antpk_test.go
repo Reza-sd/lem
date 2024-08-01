588798a87146ead3ad493b8d020e8e55a4b05c7a
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-08-01 09:31:10 +0000

    .

--- internal/antpk/0-Const-antpk_test.go
+++ internal/antpk/0-Const-antpk_test.go
@@ -11,7 +11,7 @@ import (
  */
 
 // ===============================================
-var(
+var (
 	//-------Ant type example-----------
 	ant1 = Ant{
 		Name:            "reza",
@@ -19,8 +19,15 @@ var(
 		VisitedRoomsArr: []string{"room_0", "room_1"},
 		StepNumber:      2,
 	}
+	//-------Ant type example-----------
+	ant2 = Ant{
+		Name:            "L3",
+		CurrentRoomName: "mio_5",
+		VisitedRoomsArr: []string{"mio_7", "mio_3", "mio_5"},
+		StepNumber:      3,
+	}
 	//--------AntGroup type example---------
-	AntGroup1 = AntGroup{
+	antGroup1 = AntGroup{
 		NumberOfAnts: 1,
 		AntsMap: map[int]Ant{
 			1: {Name: "Ant1",
@@ -32,41 +39,79 @@ var(
 		NumberOfSequence: 2,
 	}
 	//-------AntGroup type example------
-	AntGroup2 = AntGroup{
+	antGroup2 = AntGroup{
 		NumberOfAnts: 1,
 		AntsMap: map[int]Ant{
 			1: ant1,
 		},
 		NumberOfSequence: 2,
 	}
-	//----------
+
+	//-------AntGroup type example------
+
+	antGroup3 = AntGroup{
+		NumberOfAnts: 2,
+		AntsMap: map[int]Ant{
+			1: ant1,
+			2: ant2,
+		},
+		NumberOfSequence: 2,
+	}
+	//------------------
+	travelPlan1 = TravelPlan{
+		FinalSequence: 2,
+		TheBestPlan:   antGroup3,
+	}
+	//------------------
 )
 
-//---------------------------------------------
+// ---------------------------------------------
 func TestHello(t *testing.T) {
+	//------------
+	t.Run("0-an example of non-struct type", func(t *testing.T) {
+		//---Arrange---
+		expectation := false
+		//---Act---
+		//---Assert----
+		//assertIfStruct(t, "miio") fail test
+		assertIfStruct(t, "mio", expectation)
+	})
 	//------------
 	t.Run("1-an example of Ant struct type", func(t *testing.T) {
 		//---Arrange---
+		expectation := true
 		//---Act---
 		//---Assert----
-		assertIfStruct(t, ant1)
+		//assertIfStruct(t, "miio") fail test
+		assertIfStruct(t, travelPlan1, expectation)
 	})
 	//------------
 	t.Run("2-an example of AntGroup struct type", func(t *testing.T) {
 		//---Arrange---
+		expectation := true
 		//---Act---
 		//---Assert----
-		assertIfStruct(t, AntGroup1)
+		assertIfStruct(t, travelPlan1, expectation)
 	})
 	// ------------
+
+	t.Run("3-an example of TravelPlan struct type", func(t *testing.T) {
+		//---Arrange---
+		expectation := true
+		//---Act---
+		//---Assert----
+
+		assertIfStruct(t, travelPlan1, expectation)
+	})
+	//------------
 }
 
 // -----------------------------------------------------
-func assertIfStruct(t testing.TB, s interface{}) {
+func assertIfStruct(t testing.TB, s interface{}, expectation bool) {
 	t.Helper()
-	
+
 	v := reflect.ValueOf(s)
-	if v.Kind() != reflect.Struct {
+	if v.Kind() != reflect.Struct && expectation {
 		t.Errorf("not struct")
 	}
 
