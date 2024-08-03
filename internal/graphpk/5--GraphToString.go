55fc5839402c7d208197f98ed1e5a6ae8513c88e
Reza <136134769+Reza-sd@users.noreply.github.com>
2024-08-03 00:59:41 +0000

    .

--- internal/graphpk/5--GraphToString.go
+++ internal/graphpk/5--GraphToString.go
@@ -4,20 +4,18 @@ import (
 	"fmt"
 )
 
-func (myGraph *Graph) ToString() (string, error){
-    var graphString string
+func (myGraph *Graph) ToString() (string, error) {
+	var graphString string
 
-    graphString=fmt.Sprintf("\n Start Room=%v \n End Room=%v \n NumberOfAllRoom=%v \n CurrentAntsInEndRoom=%v\n",myGraph.StartRoomName,myGraph.EndRoomName,myGraph.NumberOfAllRoom,myGraph.CurrentAntsInEndRoom)
+	graphString = fmt.Sprintf("\nStartRoomName=%v \nEndRoomName=%v \nNumberOfAllRoom=%v \nCurrentAntsInEndRoom=%v\n", myGraph.StartRoomName, myGraph.EndRoomName, myGraph.NumberOfAllRoom, myGraph.CurrentAntsInEndRoom)
 
-    count:=0
-    for roomName :=range myGraph.Rooms {
-        count++
-        room := myGraph.Rooms[roomName]
-        roomStr := fmt.Sprintf("\n%v-Room %v: Tunnels: %v, EmptySeats: %d, MaxSeats: %d\n",count, room.Name, room.Tunnels, room.EmptySeats, room.MaxSeats)
-        graphString+=roomStr
-    }
-
-    return graphString,nil
+	count := 0
+	for roomName := range myGraph.Rooms {
+		count++
+		room := myGraph.Rooms[roomName]
+		roomStr := fmt.Sprintf("\n%v-Room= %v Tunnels= %v EmptySeats= %d MaxSeats= %d\n", count, room.Name, room.Tunnels, room.EmptySeats, room.MaxSeats)
+		graphString += roomStr
+	}
+	graphString += "\n"
+	return graphString, nil
 }
-
-
