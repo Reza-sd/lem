f644fc00ca6fe22e42f48ba673b6ba4d45aa4b61
Reza-sd <netart2000@gmail.com>
2024-08-12 20:56:31 +0100

    .

--- internal/graphpk/1--initGraph.go
+++ internal/graphpk/1--initGraph.go
@@ -3,15 +3,21 @@ package graphpk
 // "fmt"
 
 // =======================================================
-func (myGraph *Graph) InitGraph(tunnelMap map[mtg][]mtg, end mtg) error {
+func (myGraph *Graph) InitGraph(tunnelMap map[mtg][]mtg, EndRoomName mtg) error {
 	//funcName := "InitGraph"
 	// set start and end room
 	//myGraph.StartRoomName = start
-	myGraph.EndRoomName = end
+	myGraph.EndRoomName = EndRoomName
 	myGraph.CurrentAntsInEndRoom = 0
 	// Initialize rooms map
 	myGraph.RoomsDb = make(map[mtg]bool)
+	myGraph.TunnelsDb = make(map[mtg][]mtg)
 
+	myGraph.TunnelsDb = tunnelMap
+
+	for key :=range myGraph.TunnelsDb{
+		myGraph.RoomsDb[key]=true
+	}
 	//err := myGraph.setTunnelsToGraphRooms(tunnelMap)
 	// if err != nil {
 	// 	//fmt.Println("Error:", err)
