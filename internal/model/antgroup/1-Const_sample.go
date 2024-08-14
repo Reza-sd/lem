5021fd8c34182037106295c25ed6a7c581a28f36
Reza-sd <netart2000@gmail.com>
2024-08-14 20:40:01 +0100

    .

--- internal/model/antgroup/1-Const_sample.go
+++ internal/model/antgroup/1-Const_sample.go
@@ -1,30 +1,34 @@
 package antpk
 
+import (
+	//"main/internal/logstack"
+	"main/internal/model/antgroup/ant"
+)
 //"main/internal/graphpk"
 //"strings"
 
 // ==================Samples=============================
 
 // //-------Ant type example-----------
-func Sample_ant1() Ant {
+// func Sample_ant1() Ant {
 
-	return Ant{
-		CurrentRoomName: 0,
-		VisitedRoomsArr: []Mtag{0},
-		StepNumber:      0,
-	}
+// 	return Ant{
+// 		CurrentRoomName: 0,
+// 		VisitedRoomsArr: []Mtag{0},
+// 		StepNumber:      0,
+// 	}
 
-}
+// }
 
 // //-------Ant type example-----------
-func Sample_ant2() Ant {
-	return Ant{
-		//Name:            2,
-		CurrentRoomName: 0,
-		VisitedRoomsArr: []Mtag{0},
-		StepNumber:      0,
-	}
-}
+// func Sample_ant2() Ant {
+// 	return Ant{
+// 		//Name:            2,
+// 		CurrentRoomName: 0,
+// 		VisitedRoomsArr: []Mtag{0},
+// 		StepNumber:      0,
+// 	}
+// }
 
 // //-------AntGroup type example------
 
@@ -40,7 +44,7 @@ func Sample_AntGroup_1ant_initmode_room_0() AntGroup {
 	return AntGroup{
 		NumberOfAnts: 1,
 		AntsDb: map[Mtag]Ant{
-			1: Sample_ant1(),
+			1: ant.Sample_ant1(),
 		},
 		CurrentSequenceNumber: 0,
 		UsedTunnel:            make(map[Mtag][]Mtag),
@@ -53,8 +57,8 @@ func Sample_AntGroup_2ant_initmode_room_0() AntGroup {
 	return AntGroup{
 		NumberOfAnts: 2,
 		AntsDb: map[Mtag]Ant{
-			1: Sample_ant1(),
-			2: Sample_ant2(),
+			1: ant.Sample_ant1(),
+			2: ant.Sample_ant2(),
 		},
 		CurrentSequenceNumber: 0,
 		UsedTunnel:            make(map[Mtag][]Mtag),
---
+++ internal/model/antgroup/ant/1-const_sample.go
@@ -0,0 +1,23 @@
+package ant
+
+
+//-------Ant type example-----------
+func Sample_ant1() Ant {
+
+	return Ant{
+		CurrentRoomName: 0,
+		VisitedRoomsArr: []Mta{0},
+		StepNumber:      0,
+	}
+
+}
+//----------------
+func Sample_ant2() Ant {
+	return Ant{
+		//Name:            2,
+		CurrentRoomName: 0,
+		VisitedRoomsArr: []Mta{0},
+		StepNumber:      0,
+	}
+}
+//------------------------
