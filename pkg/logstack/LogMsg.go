7f1a918c8a3773e0cd2551f460ba319fa10c6e35
Reza-sd <netart2000@gmail.com>
2024-08-16 16:00:53 +0100

    .

--- pkg/logstack/LogMsg.go
+++ pkg/logstack/LogMsg.go
@@ -1,26 +1,31 @@
 package logstack
 
+import "fmt"
+
 //import "go/types"
 
 // ----------------------------------------
 func LogMsg(packageName string, FuncName string, OperationName string, description string, RetunedError interface{}) (string, string, string, string, string, string, string, string, string) {
 
-	var errStr string
-	errStr = "nil"
-	//var Rerr error
+	
+	//errString := "nil"
+	errString:=fmt.Sprintf("%v",RetunedError)
 
-	switch RetunedError.(type) {
-	case string:
-		errStr = RetunedError.(string)
-	case error:
-		if RetunedError.(error) != nil {
-			errStr = RetunedError.(string)
-		}
+	// switch v := RetunedError.(type) {
+	// case string:
+	// 	if v!=""{
+	// 		errString = v
+	// 	}
+		
+	// case error:
+	// 	if v != nil {
+	// 		errString = v.Error()
+	// 	} 
 
-	}
+	// }
 
-	return description, "package", packageName, "func", FuncName, "op", OperationName, "error", errStr
+	return description, "package", packageName, "func", FuncName, "op", OperationName, "error", errString
 
 }
 
-//--------------------
+//----------------------------------------------
