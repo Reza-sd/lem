947136af98bde38e75c385a546d6482c1d092a08
Reza-sd <netart2000@gmail.com>
2024-08-18 07:37:07 +0100

    .

--- pkg/logstack/3---------RSlogErr.go
+++ pkg/logstack/3---------RSlogErr.go
@@ -5,7 +5,7 @@ import (
 )
 
 // -------------------RSlogErr-------------------------
-func RSlogErr(packageName, funcName, opName, opDes string, err error) error {
+func RSlogErr(packageName, funcName, opName, opDes string, err interface{}) error {
 	msg, agrs := LogMsgGenerator(packageName, funcName, opName, opDes, err)
 
 	slog.Error(msg, agrs)
---
+++ pkg/logstack/t-3---------RSlogErr_test.go
@@ -0,0 +1,17 @@
+package logstack
+
+import (
+	"fmt"
+	"testing"
+)
+
+//-----------------------------------------------------
+func Test_RSlogErr(t *testing.T) {
+
+	t.Run(`1-RSlogErr`, func(t *testing.T) {
+		errMsg:= RSlogErr("logstackpk","RSlogErr-fn","RSlogErr","return slog when logger stack fail","error")
+		fmt.Printf("%v\n",errMsg)
+		//SampleLogger.ErrLog("LogMsgGenerator", "OperationName", "errMsg", "operationDescription")
+	})
+}
+//-----------------------------------------------------
