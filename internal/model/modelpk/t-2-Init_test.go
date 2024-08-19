72ec1279a13b3e41b5c5e9f75f19ab447d0b9788
Reza-sd <netart2000@gmail.com>
2024-08-19 16:16:44 +0100

    .

--- internal/model/modelpk/t-2-Init_test.go
+++ internal/model/modelpk/t-2-Init_test.go
@@ -6,21 +6,21 @@ import (
 
 func Test_Init(t *testing.T) {
 
-	t.Run(`1-Print`, func(t *testing.T) {
+	t.Run(`1-Print-Sample_Lem_1ant_2room`, func(t *testing.T) {
 		myLem := mySampleModel.Sample_Lem_1ant_2room()
 
 		var model1 Model
 		model1.Init(&myLem)
 		model1.Print()
 	})
-	t.Run(`2-Print`, func(t *testing.T) {
+	t.Run(`2-Print-Sample_Lem_2ant_2room`, func(t *testing.T) {
 		myLem := mySampleModel.Sample_Lem_2ant_2room()
 
 		var model1 Model
 		model1.Init(&myLem)
 		model1.Print()
 	})
-	t.Run(`2-Print`, func(t *testing.T) {
+	t.Run(`2-Print-Sample_Lem_2ant_3room`, func(t *testing.T) {
 		myLem := mySampleModel.Sample_Lem_2ant_3room()
 
 		var model1 Model
