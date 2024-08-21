10f4779ecd63dae5f0f7b106a6cc762eb5247a67
Reza-sd <netart2000@gmail.com>
2024-08-21 14:18:59 +0100

    .

--- internal/model/graph/2-1-init.go
+++ internal/model/graph/2-1-init.go
@@ -8,7 +8,7 @@ func (myGraph *Graph) Init(myTunnelMap TunnelMap, EndRoomName Mtg) error {
 
 	myGraph.RoomAvailableDb = make(map[Mtg]bool)
 
-	myGraph.TunnelsDb = &myTunnelMap
+	myGraph.TunnelsDb = &myTunnelMap //?<<----?????
 
 	for key := range *myGraph.TunnelsDb {
 		myGraph.RoomAvailableDb[key] = true
