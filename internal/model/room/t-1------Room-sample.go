6b137b152c2c653df20b1b76da5c9e24f0052343
Reza-sd <netart2000@gmail.com>
2024-08-22 12:19:24 +0100

    .

--- internal/model/room/t-1------Room-sample.go
+++ internal/model/room/t-1------Room-sample.go
@@ -2,7 +2,7 @@ package room
 
 type SampleRoom struct{}
 
-func (s *SampleRoom) Sample_Room_1_5000_1000() *Room {
+func (s *SampleRoom) Sample_Room_End() *Room {
 
 	return &Room{
 		Name:            1,
@@ -11,3 +11,25 @@ func (s *SampleRoom) Sample_Room_1_5000_1000() *Room {
 		ConnectionSlice: []Mtr{2, 3, 4},
 	}
 }
+
+// ----------------
+func (s *SampleRoom) Sample_Room_Start_name_0() *Room {
+
+	return &Room{
+		Name:            0,
+		AllSeats:        5000,
+		UsedSeats:       1000,
+		ConnectionSlice: []Mtr{1, 2, 3},
+	}
+}
+// ----------------
+func (s *SampleRoom) Sample_Room_Middle() *Room {
+
+	return &Room{
+		Name:            3,
+		AllSeats:        1,
+		UsedSeats:       0,
+		ConnectionSlice: []Mtr{2, 4},
+	}
+}
+// ----------------
