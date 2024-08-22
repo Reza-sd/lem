7d38ab021b01ac44667e18753dba62f3023b9f3e
Reza-sd <netart2000@gmail.com>
2024-08-22 11:47:44 +0100

    .

--- internal/model/room/t-1------Room-sample.go
+++ internal/model/room/t-1------Room-sample.go
@@ -6,9 +6,9 @@ type SampleRoom struct{}
 func (s *SampleRoom) Sample_Room_1 () *Room {
 
 	return &Room{
-		Name: Mtr(1),
-		AllSeats: Mtr(100),
-		UsedSeats: Mtr(0),
+		Name: 1,
+		AllSeats: 100,
+		UsedSeats: 50,
 		ConnectionSlice: []Mtr{2,3,4},
 	}
 }
