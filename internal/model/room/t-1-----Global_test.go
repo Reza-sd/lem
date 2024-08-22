83202cf270f95ff9182dabd79c25458fbd7bd7cc
Reza-sd <netart2000@gmail.com>
2024-08-22 15:07:07 +0100

    .

--- internal/model/room/t-1-----Global_test.go
+++ internal/model/room/t-1-----Global_test.go
@@ -7,16 +7,25 @@ import (
 )
 
 // ========================================
-func Test_InitGraph(t *testing.T) {
+func Test_RoomPk(t *testing.T) {
 	for i := 0; i < len(AllTestCasesPkSlice); i++ {
 		//fmt.Println(mySlice[i])
 		for j := 0; j < len(AllTestCasesPkSlice[i].TestCases); j++ {
+
 			NumfuncDes := fmt.Sprintf("%v-%v-%v", j+1, AllTestCasesPkSlice[i].FuncName, AllTestCasesPkSlice[i].TestCases[j].Des)
 
 			t.Run(NumfuncDes, func(t *testing.T) {
-
+				if AllTestCasesPkSlice[i].TestCases[j].Skip {
+					t.Skip()
+				}
 				got := AllTestCasesPkSlice[i].TestCases[j].got
+				//print(got)
 				//a :=*got
+				//fmt.Println("***>",got)
+				// switch got.(type){
+				// case func()any:
+				// 	fmt.Println("BBBBBBBB")
+				// }
 				exp := AllTestCasesPkSlice[i].TestCases[j].exp
 				Assert(t, got, exp)
 			})
@@ -28,10 +37,27 @@ func Test_InitGraph(t *testing.T) {
 // ==================================
 func Assert(t testing.TB, got, exp any) {
 	t.Helper()
-	if !reflect.DeepEqual(got, exp) {
+
+	var gotValue any
+	var expValue any
+
+	switch got.(type) {
+	case func() any:
+		gotValue = got.(func() any)()
+	default:
+		gotValue = got
+	}
+
+	switch exp.(type) {
+	case func() any:
+		expValue = exp.(func() any)()
+	default:
+		expValue = exp
+	}
+
+	if !reflect.DeepEqual(gotValue, expValue) {
 		//t.Fatalf("\n>>>not same: \n got=%v<<\n exp=%v<<", got, exp)
 		t.Errorf("\n>>>not same: \n got=%v<<\n exp=%v<<", got, exp)
-		//print(TT.Des)
 	}
 }
 
--- internal/model/room/t-2-4-Init.go
+++ internal/model/room/t-2-4-Init.go
@@ -6,16 +6,45 @@ var Init_Test = TestCasesFunc{
 		{
 			Des: "test1111",
 			got: func() any {
-				a := 1
-				b := 3
-				c := b - a
-				return Mtr(c)
+				// a := 1
+				// b := 3
+				// c := b - a
+				return 3
 			},
-			exp: Mtr(3),
+			exp: 3,
 		},
 		{
+			//Skip: true,
 			Des: "test 22222",
 			got: 4,
 			exp: 4,
+		},
+		{
+			//Skip: true,
+			Des: "test 3333",
+			got: "mio",
+			exp: "mio",
+		},
+		{
+			//Skip: true,
+			Des: "test 3333",
+			got: 5,
+			exp: func() any {
+				a := 1
+				b := 6
+				c := b - a
+				return c
+			},
+		},
+		{
+			//Skip: true,
+			Des: "test 3333",
+			got: true,
+			exp: func() any {
+				a := 1
+				b := 6
+				c := b - a
+				return c == 5
+			},
 		},
 	}}
