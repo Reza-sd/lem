5fe2727e869d0a11203adf3b20aadf4e0f34d949
Reza-sd <netart2000@gmail.com>
2024-08-22 12:14:29 +0100

    .

--- internal/model/room/t-3------Print_test.go
+++ internal/model/room/t-3------Print_test.go
@@ -3,6 +3,7 @@ package room
 import "testing"
 
 func Test_Print(t *testing.T) {
+	t.Skip()
 	t.Run("1-", func(t *testing.T) {
 		sample := SampleRoom{}
 		myRoom := sample.Sample_Room_1_5000_1000()
