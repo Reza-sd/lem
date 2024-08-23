328c03fa277a972eab903c144fa13990482013e6
Reza-sd <netart2000@gmail.com>
2024-08-23 11:18:08 +0100

    .

--- internal/model/room/2-2-OneComeUpdateSeats.go
+++ internal/model/room/2-2-OneComeUpdateSeats.go
@@ -2,6 +2,7 @@ package room
 
 import "fmt"
 
+// =====================================================
 func (myRoom *Room) OneComeUpdateSeats() error {
 
 	if myRoom.UsedSeats == myRoom.AllSeats {
@@ -15,3 +16,5 @@ func (myRoom *Room) OneComeUpdateSeats() error {
 	myRoom.UsedSeats++
 	return nil
 }
+
+//=====================================================
