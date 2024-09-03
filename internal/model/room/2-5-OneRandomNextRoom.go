e7c98c30f1b4737ab6d2a3127393c6fb548c6c59
Reza-sd <netart2000@gmail.com>
2024-09-03 11:03:41 +0100

    .

--- internal/model/room/2-5-OneRandomNextRoom.go
+++ internal/model/room/2-5-OneRandomNextRoom.go
@@ -6,17 +6,16 @@ import (
 	//"crypto/rand"
 )
 
-func (myRoom *Room) OneRandomNextRoom() (Mtr, error) {
+func (r *Room) OneRandomNextRoom() (Mtr, error) {
 	//--------------------------------------
-	lenConnectionSlice := len(myRoom.connectionSlice)
+	lenConnectionSlice := len(r.getConnectionSlice())
 	if lenConnectionSlice == 0 {
 		return Mtr(0), fmt.Errorf("empty")
 	}
-	//----------------------------------------
-	var nextRandomRoomName Mtr
 	//-------------------------------------
 	randomNextRoomIndex := rand.Intn(lenConnectionSlice)
-	nextRandomRoomName = myRoom.connectionSlice[randomNextRoomIndex]
+
+	nextRandomRoomName := r.getConnectionSlice()[randomNextRoomIndex]
 	//-----------------------
 	return nextRandomRoomName, nil
 }
--- internal/model/room/3-----Print.go
+++ internal/model/room/3-----Print.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func (r *Room) Print() {
 
-	fmt.Printf("\nRoom: Name=%v, AllSeats=%v, UsedSeats=%v, ConnectionSlice=%v\n", r.name, r.allSeats, r.usedSeats, r.connectionSlice)
+	fmt.Printf("\nRoom: Name=%v, AllSeats=%v, UsedSeats=%v, ConnectionSlice=%v\n", r.getName(), r.getAllSeats(), r.getUsedSeats(), r.getConnectionSlice())
 
 }
