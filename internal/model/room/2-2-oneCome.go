43bcecf4ba2313359dbfba415c534d9893b79c63
Reza-sd <netart2000@gmail.com>
2024-09-03 20:15:03 +0100

    .

--- internal/model/room/2-2-oneCome.go
+++ internal/model/room/2-2-oneCome.go
@@ -5,15 +5,15 @@ package room
 // =====================================================
 func (u *updater) oneCome() *updater {
 	//u.room
-	if u.room.get.usedSeats() == u.room.get.allSeats() {
+	if u.room.get.usedSeats() == u.room.get.allSeats()|| u.room.get.usedSeats()+1 > u.room.get.allSeats() {
 		//return fmt.Errorf("full")
 		return u
 	}
 
-	if u.room.get.usedSeats()+1 > u.room.get.allSeats() {
-		//return fmt.Errorf("full")
-		return u
-	}
+	// if u.room.get.usedSeats()+1 > u.room.get.allSeats() {
+	// 	//return fmt.Errorf("full")
+	// 	return u
+	// }
 
 	//r.usedSeats++
 	u.room.set.usedSeats(u.room.get.usedSeats() + 1)
