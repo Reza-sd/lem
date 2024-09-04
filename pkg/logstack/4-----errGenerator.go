39dcde266cac7d1d1e936219aecc6839dd584a39
Reza-sd <netart2000@gmail.com>
2024-09-04 17:20:54 +0100

    .

--- pkg/logstack/4-----errGenerator.go
+++ pkg/logstack/4-----errGenerator.go
@@ -4,12 +4,15 @@ import "fmt"
 
 // ----------------------------------------
 func errGenerator(FuncName string, Operation string, RetunedError any) error {
+	RetunedErrorErrorType := fmt.Errorf("%v",RetunedError)
 
-	return fmt.Errorf("<==={%v}--%v<---%v[%v]***", pkgName, FuncName, Operation, RetunedError)
+	 // wrapping RetunedError using %w format specifier and adding more context 
+
+
+	return fmt.Errorf("<==={%v}--%v<---%v[%w]***", pkgName, FuncName, Operation, RetunedErrorErrorType)
 	//If error is not nil, it is their responsibility to check for it and handle it (log, return, serve, invoke some retry/cleanup mechanism, etc.).
 
 	//errors.Unwrap
-	
 }
 
 // ----------------------------------------
