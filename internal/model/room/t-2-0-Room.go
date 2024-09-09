ba530a495668d3091001b5d883a3d5417e62cefa
Reza-sd <netart2000@gmail.com>
2024-09-09 17:43:31 +0100

    .

--- internal/model/room/t-2-0-Room.go
+++ internal/model/room/t-2-0-Room.go
@@ -1,13 +1,6 @@
 package room
 
-import (
-	ts "main/pkg/teststack"
-)
-
 // ====================================
-
-type TestCasesForFunc = ts.AllCasesFunc
-type TestCase = ts.Case
 
 type SampleRoomSt struct{}
 
@@ -41,43 +34,3 @@ func (s *SampleRoomSt) Middle_Name_3() *room {
 }
 
 // ====================================================================
-var test_roomBuilder_Cases = TestCasesForFunc{
-	FuncName: "Room.roomBuilder",
-	//Skip: true,
-	TestCases: []TestCase{
-		//---------------------------------------
-		{
-			Des: "if name=startRoomName means start home",
-			Got: newRuledRoom(rmBuildArg{name: startRoomName, endRoomName: 5, connectionSlice: []RT{1, 2, 3}}),
-			Exp: newPlainRoom().
-				SetName(startRoomName).
-				SetAllSeats(MaxSeatsStartEnd).
-				SetUsedSeats(UsedSeatsStartEnd).
-				SetConnectionSlice([]RT{1, 2, 3}),
-		},
-		//---------------------------------------
-		{
-			//Skip: true,
-			Des: "non start or end room",
-			Got: newRuledRoom(rmBuildArg{name: 3, endRoomName: 5, connectionSlice: []RT{1, 2, 3}}),
-			Exp: newPlainRoom().
-				SetName(3).
-				SetAllSeats(1).
-				SetUsedSeats(0).
-				SetConnectionSlice([]RT{1, 2, 3}),
-		},
-		//---------------------------------------
-		{
-			//Skip: true,
-			Des: "end room",
-			Got: newRuledRoom(rmBuildArg{name: 5, endRoomName: 5, connectionSlice: []RT{1, 2, 3}}),
-			Exp: newPlainRoom().
-				SetName(5).
-				SetAllSeats(MaxSeatsStartEnd).
-				SetUsedSeats(UsedSeatsStartEnd).
-				SetConnectionSlice([]RT{1, 2, 3}),
-		},
-		//---------------------------------------
-	}}
-
-//====================================================================
