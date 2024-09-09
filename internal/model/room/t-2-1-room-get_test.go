ef5dcb2d6be5fcb412600274d8ce09c83244480e
Reza-sd <netart2000@gmail.com>
2024-09-10 00:22:16 +0100

    .

--- internal/model/room/t-2-1-room-get_test.go
+++ internal/model/room/t-2-1-room-get_test.go
@@ -14,4 +14,11 @@ func Test_hasOneFreeSeat(t *testing.T) {
 		//fmt.Println(inp)
 		assert(t, got, exp, inp1)
 	})
+	t.Run("1-hasOneFreeSeat", func(t *testing.T) {
+		inp1 := SampleRoom.Middle_Name_3()
+		got := inp1.get.hasOneFreeSeat()
+		exp := true
+		//fmt.Println(inp)
+		assert(t, got, exp, inp1)
+	})
 }
