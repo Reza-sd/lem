1732e0ab7c7c5381797ba67e35aee0425bc99335
Reza-sd <netart2000@gmail.com>
2024-09-16 18:29:28 +0100

    .

---
+++ app/golang-example/interface6/interface6.go
@@ -0,0 +1,35 @@
+package main
+
+type Logger interface {
+    Log(message string)
+}
+
+type Service interface {
+    DoSomething() error
+}
+
+type ConsoleLogger struct{}
+
+func (l *ConsoleLogger) Log(message string) {
+    println(message)
+}
+//-------
+type MyService struct {
+    logger Logger
+}
+
+func (s *MyService) DoSomething() error {
+    s.logger.Log("Doing something")
+    // Implementation
+    return nil
+}
+//------------
+func NewMyService(logger Logger) Service {
+    return &MyService{logger: logger}
+}
+// Usage
+func main() {
+   logger := &ConsoleLogger{}
+    service := NewMyService(logger)
+    service.DoSomething()
+}
