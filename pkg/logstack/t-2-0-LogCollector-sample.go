67c565185fe83cd1b98d48ccb41b571c386bfa1a
Reza-sd <netart2000@gmail.com>
2024-09-23 17:45:01 +0100

    .

--- pkg/logstack/t-2-0-LogCollector-sample.go
+++ pkg/logstack/t-2-0-LogCollector-sample.go
@@ -5,7 +5,7 @@ var errCodeDes1 = map[errT]string{
 	11: "11-is not ok",
 	12: "12-cant handle user",
 }
-var sampleLogger2 = BuildNewLogger(pkgName, errCodeDes1, true, true)
+//var sampleLogger2 = BuildNewLogger(pkgName, errCodeDes1, true, true)
 
 var sampleLogger1 = BuildNewLogger(pkgName, errCodeDes1, true, true)
 
