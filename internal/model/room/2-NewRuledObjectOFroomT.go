0194eecb4d220218915d149534703d8d9b846c1b
Reza-sd <netart2000@gmail.com>
2024-10-16 00:28:41 +0100

    .

--- internal/model/room/2-NewRuledObjectOFroomT.go
+++ internal/model/room/2-NewRuledObjectOFroomT.go
@@ -1,40 +1,44 @@
 package room
 
 // ---------------------------------------
-func NewRuledObjectOFroomT(name_param rT, connectionSlice_param []rT, isEndroom_param bool) (*roomT, []errT) { //Constructor=factory function=builder
-	room := newPlainRoom()
+func Return_NewRuledObjectOFroomT(name_param rT, connectionSlice_param []rT, isEndroom_param bool) (*roomT, []errT) { //Constructor=factory function=builder
+	myRoom := return_NewPlainRoom()
 
-	if err_set_name := room.set.name(name_param); err_set_name != nil {
+	if err_set_name := myRoom.set.name(name_param); err_set_name != nil {
 		return nil, logger.Act.Err.Rlog(_ERR_NewRuledRoom_10, err_set_name, "set.name(name):", "name:", name_param)
 	}
-	if err_set_connectionSlice := room.set.connectionSlice(connectionSlice_param); err_set_connectionSlice != nil {
+
+	if err_set_connectionSlice := myRoom.set.connectionSlice(connectionSlice_param); err_set_connectionSlice != nil {
 		return nil, logger.Act.Err.Rlog(_ERR_NewRuledRoom_20, err_set_connectionSlice, "r.set.connectionSlice")
 	}
 
 	//if the room is start or end room
 	if name_param == _START_ROOM_NAME || isEndroom_param {
 
-		if err_set_allSeats := room.set.allSeats(_MAX_SEATS_START_END); err_set_allSeats != nil {
+		if err_set_allSeats := myRoom.set.allSeats(_MAX_SEATS_START_END); err_set_allSeats != nil {
 			return nil, logger.Act.Err.Rlog(_ERR_NewRuledRoom_30, err_set_allSeats, "r.set.allSeats")
 		}
-		if err_set_usedSeats := room.set.usedSeats(_USED_SEATS_START_END); err_set_usedSeats != nil {
+
+		if err_set_usedSeats := myRoom.set.usedSeats(_USED_SEATS_START_END); err_set_usedSeats != nil {
 			return nil, logger.Act.Err.Rlog(_ERR_NewRuledRoom_40, err_set_usedSeats, "r.set.usedSeats")
 		}
+
 	} else {
-		if err_set_allSeats := room.set.allSeats(_ALL_SEATS_NORMAL_ROOM); err_set_allSeats != nil {
+		if err_set_allSeats := myRoom.set.allSeats(_ALL_SEATS_NORMAL_ROOM); err_set_allSeats != nil {
 			return nil, logger.Act.Err.Rlog(_ERR_NewRuledRoom_50, err_set_allSeats, "r.set.allSeats")
 		}
-		if err_set_usedSeats := room.set.usedSeats(_EMPTY_USED_SEATS); err_set_usedSeats != nil {
+
+		if err_set_usedSeats := myRoom.set.usedSeats(_EMPTY_USED_SEATS); err_set_usedSeats != nil {
 			return nil, logger.Act.Err.Rlog(_ERR_NewRuledRoom_60, err_set_usedSeats, "r.set.usedSeats(_EMPTY_USED_SEATS)", "_EMPTY_USED_SEATS=", _EMPTY_USED_SEATS)
 		}
 	}
 
-	return room, nil
+	return myRoom, nil
 
 }
 
 // ---------------------------------------------------
-func newPlainRoom() *roomT { //Constructor=factory function=builder
+func return_NewPlainRoom() *roomT { //Constructor=factory function=builder
 
 	r := &roomT{}
 
--- internal/model/room/t-2-newRuledRoom_test.go
+++ internal/model/room/t-2-newRuledRoom_test.go
@@ -8,28 +8,28 @@ func Test_newRuledRoom(t *testing.T) {
 	//t.Skip()
 	t.Run("1-NewRuledRoom", func(t *testing.T) {
 		//inp1 := SampleRoom.Middle_Name_3()
-		got, _ := NewRuledObjectOFroomT(_START_ROOM_NAME, nil, false)
+		got, _ := Return_NewRuledObjectOFroomT(_START_ROOM_NAME, nil, false)
 		got.Act.Print()
 		//exp := false
 		//logger.Assert(t, got, exp, inp1)
 	})
 	t.Run("2-NewRuledRoom", func(t *testing.T) {
-		got, _ := NewRuledObjectOFroomT(5, nil, true)
+		got, _ := Return_NewRuledObjectOFroomT(5, nil, true)
 		got.Act.Print()
 
 	})
 	t.Run("3-NewRuledRoom", func(t *testing.T) {
-		got, _ := NewRuledObjectOFroomT(1, nil, false)
+		got, _ := Return_NewRuledObjectOFroomT(1, nil, false)
 		got.Act.Print()
 
 	})
 	t.Run("4-NewRuledRoom", func(t *testing.T) {
-		got, _ := NewRuledObjectOFroomT(1, []rT{7, 3, 14}, false)
+		got, _ := Return_NewRuledObjectOFroomT(1, []rT{7, 3, 14}, false)
 		got.Act.Print()
 	})
 	t.Run("5-NewRuledRoom exceed maxname", func(t *testing.T) {
 		inp1 := rT(_MAX_NAME + 1)
-		_, gotErr := NewRuledObjectOFroomT(inp1, []rT{7, 3, 14}, false)
+		_, gotErr := Return_NewRuledObjectOFroomT(inp1, []rT{7, 3, 14}, false)
 		expErr := []errT{_ERR_roomT_setT_name_10, _ERR_NewRuledRoom_10}
 		logger.Helper.Assert(t, gotErr, expErr, inp1)
 	})
