bcc353b604ccc56fb7a9ce813def8166f4a07149
Reza-sd <netart2000@gmail.com>
2024-10-23 10:00:50 +0100

    .

---
+++ app/designPatterns/creational/Abstract-Factory/eg-3/abstract-factory-3.go
@@ -0,0 +1,88 @@
+package main
+
+import "fmt"
+
+// Abstract Products
+type Button interface {
+    Render() string
+}
+
+type Checkbox interface {
+    Render() string
+}
+
+// Concrete Products for Light Theme
+type LightButton struct{}
+
+func (b *LightButton) Render() string {
+    return "Rendering a light button with white background"
+}
+
+type LightCheckbox struct{}
+
+func (c *LightCheckbox) Render() string {
+    return "Rendering a light checkbox with white background"
+}
+
+// Concrete Products for Dark Theme
+type DarkButton struct{}
+
+func (b *DarkButton) Render() string {
+    return "Rendering a dark button with black background"
+}
+
+type DarkCheckbox struct{}
+
+func (c *DarkCheckbox) Render() string {
+    return "Rendering a dark checkbox with black background"
+}
+
+// Abstract Factory interface
+type UIFactory interface {
+    CreateButton() Button
+    CreateCheckbox() Checkbox
+}
+
+// Concrete Factories
+type LightThemeFactory struct{}
+
+func (f *LightThemeFactory) CreateButton() Button {
+    return &LightButton{}
+}
+
+func (f *LightThemeFactory) CreateCheckbox() Checkbox {
+    return &LightCheckbox{}
+}
+
+type DarkThemeFactory struct{}
+
+func (f *DarkThemeFactory) CreateButton() Button {
+    return &DarkButton{}
+}
+
+func (f *DarkThemeFactory) CreateCheckbox() Checkbox {
+    return &DarkCheckbox{}
+}
+
+// Client code
+func createUI(factory UIFactory) {
+    button := factory.CreateButton()
+    checkbox := factory.CreateCheckbox()
+
+    fmt.Println(button.Render())
+    fmt.Println(checkbox.Render())
+}
+
+func main() {
+    // Create UI with light theme
+    lightFactory := &LightThemeFactory{}
+    fmt.Println("Creating Light Theme UI:")
+    createUI(lightFactory)
+
+    fmt.Println()
+
+    // Create UI with dark theme
+    darkFactory := &DarkThemeFactory{}
+    fmt.Println("Creating Dark Theme UI:")
+    createUI(darkFactory)
+}
