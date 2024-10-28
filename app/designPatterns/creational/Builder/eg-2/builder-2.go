16f447d062ad9b9f6dc1a9a33110708f3b576272
Reza-sd <netart2000@gmail.com>
2024-10-28 14:12:37 +0000

    .

--- app/designPatterns/creational/Builder/eg-2/builder-2.go
+++ app/designPatterns/creational/Builder/eg-2/builder-2.go
@@ -1,14 +1,14 @@
 package main
 
 import "fmt"
-
+//--------------------------------------
 // Product represents the final object being constructed.
 type Product struct {
 	PartA string
 	PartB string
 	PartC string
 }
-
+//--------------------------------------
 // Builder interface defines the common methods for constructing the Product.
 type Builder interface {
 	BuildPartA(partA string) Builder
@@ -17,7 +17,7 @@ type Builder interface {
 
 	GetProduct() Product
 }
-
+//--------------------------------------
 // ConcreteBuilderA implements the Builder interface and constructs a Product with specific parts.
 type ConcreteBuilderA struct {
 	product *Product
@@ -41,7 +41,30 @@ func (b *ConcreteBuilderA) BuildPartC(partC string) Builder {
 func (b *ConcreteBuilderA) GetProduct() Product {
 	return *b.product
 }
+//---------------------------------------
+type ConcreteBuilderB struct {
+	product *Product
+}
 
+func (b *ConcreteBuilderB) BuildPartA(partA string) Builder {
+	b.product.PartA = partA+" B"
+	return b
+}
+
+func (b *ConcreteBuilderB) BuildPartB(partB string) Builder {
+	b.product.PartB = partB+" B"
+	return b
+}
+
+func (b *ConcreteBuilderB) BuildPartC(partC string) Builder {
+	b.product.PartC = partC+" B"
+	return b
+}
+
+func (b *ConcreteBuilderB) GetProduct() Product {
+	return *b.product
+}
+//------------------------------------------
 // Director orchestrates the construction process, calling methods on the builder.
 type Director struct{}
 
@@ -50,7 +73,7 @@ func (d *Director) Construct(builder Builder) {
 	builder.BuildPartB("Part B")
 	builder.BuildPartC("Part C")
 }
-
+//---------------------------------------
 func main() {
 	// Create a concrete builder and use it to construct a Product.
 	builder := &ConcreteBuilderA{product: &Product{}}
@@ -58,5 +81,12 @@ func main() {
 	director.Construct(builder)
 
 	product := builder.GetProduct()
+	fmt.Println("Product:", product)
+
+	//---------------------
+	//OR
+	builder2 :=&ConcreteBuilderB{product: &Product{}}
+	director.Construct(builder2)
+	product = builder.GetProduct()
 	fmt.Println("Product:", product)
 }
