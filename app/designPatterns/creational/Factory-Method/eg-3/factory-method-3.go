1d5ef310c593da3980fe14bad682a0fa3a98a928
Reza-sd <netart2000@gmail.com>
2024-10-28 12:35:05 +0000

    .

--- app/designPatterns/creational/Factory-Method/eg-3/factory-method-3.go
+++ app/designPatterns/creational/Factory-Method/eg-3/factory-method-3.go
@@ -1,12 +1,12 @@
 package main
 
 import "fmt"
-
+//-------------------------------------------
 // Product interface defines the common behavior for objects to be created.
 type Product interface {
 	DoSomething()
 }
-
+//-------------------------------------------
 // ConcreteProductA implements the Product interface.
 type ConcreteProductA struct{}
 
@@ -20,12 +20,12 @@ type ConcreteProductB struct{}
 func (b *ConcreteProductB) DoSomething() {
 	fmt.Println("ConcreteProductB: Doing something.")
 }
-
+//-------------------------------------------
 // Factory interface defines the method for creating Product instances.
 type Factory interface {
 	CreateProduct() Product
 }
-
+//-------------------------------------------
 // ConcreteFactoryA implements the Factory interface and creates ConcreteProductA instances.
 type ConcreteFactoryA struct{}
 
@@ -39,7 +39,7 @@ type ConcreteFactoryB struct{}
 func (b *ConcreteFactoryB) CreateProduct() Product {
 	return &ConcreteProductB{}
 }
-
+//-------------------------------------------
 func main() {
 	// Use ConcreteFactoryA to create a ConcreteProductA instance.
 	factoryA := &ConcreteFactoryA{}
