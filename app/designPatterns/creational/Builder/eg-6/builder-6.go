73782beb11c43e903d97cfe86ea3f1bc1808f49b
Reza-sd <netart2000@gmail.com>
2024-10-30 19:35:38 +0000

    .

--- app/designPatterns/creational/Builder/eg-6/builder-6.go
+++ app/designPatterns/creational/Builder/eg-6/builder-6.go
@@ -35,18 +35,19 @@ type house_struct struct {
 //===================================
 // 2- Builder interface
 type Builder_interface interface {
-	setWindowType()
-	setDoorType()
-	setNumFloor()
-	buildHouse() *house_struct
+	set_WindowType()
+	set_DoorType()
+	set_NumFloor()
+
+	get_House() *house_struct
 }
 
 //===================================
 // 3- Concrete Builders
 //builder A
 type normalBuilder_struct struct {
-	windowType_string string
-	doorType_string   string
+	window_string string
+	door_string   string
 	floor_uint8      uint8
 }
 
@@ -54,22 +55,22 @@ func newNormalBuilder() *normalBuilder_struct {
 	return new(normalBuilder_struct)
 }
 
-func (b *normalBuilder_struct) setWindowType() {
-	b.windowType_string = windowType_Map_builderType_uint8_string[_NORMAL_BUILDER]
+func (b *normalBuilder_struct) set_WindowType() {
+	b.window_string = windowType_Map_builderType_uint8_string[_NORMAL_BUILDER]
 }
 
-func (b *normalBuilder_struct) setDoorType() {
-	b.doorType_string = doorType_Map_builderType_uint8_string[_NORMAL_BUILDER]
+func (b *normalBuilder_struct) set_DoorType() {
+	b.door_string = doorType_Map_builderType_uint8_string[_NORMAL_BUILDER]
 }
 
-func (b *normalBuilder_struct) setNumFloor() {
+func (b *normalBuilder_struct) set_NumFloor() {
 	b.floor_uint8 =uint8(2)
 }
 
-func (b *normalBuilder_struct) buildHouse() *house_struct {
+func (b *normalBuilder_struct) get_House() *house_struct {
 	h := new(house_struct)
-	h.door_string = b.doorType_string
-	h.window_string = b.windowType_string
+	h.door_string = b.door_string
+	h.window_string = b.window_string
 	h.floor_uint8 = b.floor_uint8
 	return h
 }
@@ -77,8 +78,8 @@ func (b *normalBuilder_struct) buildHouse() *house_struct {
 //----------------------
 //builder B
 type iglooBuilder_struct struct {
-	windowType_string string
-	doorType_string   string
+	window_string string
+	door_string   string
 	floor_uint8      uint8
 }
 
@@ -86,22 +87,22 @@ func newIglooBuilder() *iglooBuilder_struct {
 	return new(iglooBuilder_struct)
 }
 
-func (b *iglooBuilder_struct) setWindowType() {
-	b.windowType_string = windowType_Map_builderType_uint8_string[_IGLOO_BUILDER]
+func (b *iglooBuilder_struct) set_WindowType() {
+	b.window_string = windowType_Map_builderType_uint8_string[_IGLOO_BUILDER]
 }
 
-func (b *iglooBuilder_struct) setDoorType() {
-	b.doorType_string = doorType_Map_builderType_uint8_string[_IGLOO_BUILDER]
+func (b *iglooBuilder_struct) set_DoorType() {
+	b.door_string = doorType_Map_builderType_uint8_string[_IGLOO_BUILDER]
 }
 
-func (b *iglooBuilder_struct) setNumFloor() {
+func (b *iglooBuilder_struct) set_NumFloor() {
 	b.floor_uint8 = uint8(1)
 }
 
-func (b *iglooBuilder_struct) buildHouse() *house_struct {
+func (b *iglooBuilder_struct) get_House() *house_struct {
 	h := new(house_struct)
-	h.door_string = b.doorType_string
-	h.window_string = b.windowType_string
+	h.door_string = b.door_string
+	h.window_string = b.window_string
 	h.floor_uint8 = b.floor_uint8
 
 	return h
@@ -113,30 +114,30 @@ type director_struct struct {
 	builder Builder_interface
 }
 
-func newDirector(b Builder_interface) *director_struct {
+func new_Director(b Builder_interface) *director_struct {
 	d := new(director_struct)
 	d.builder = b
 	return d
 }
 
-func (d *director_struct) setBuilder(b Builder_interface) {
+func (d *director_struct) set_Builder(b Builder_interface) {
 	d.builder = b
 }
 
-func (d *director_struct) buildHouse() *house_struct {
-	d.builder.setDoorType()
-	d.builder.setWindowType()
-	d.builder.setNumFloor()
-	return d.builder.buildHouse()
+func (this_director *director_struct) buildHouse() *house_struct {
+	this_director.builder.set_DoorType()
+	this_director.builder.set_WindowType()
+	this_director.builder.set_NumFloor()
+	return this_director.builder.get_House()
 }
 
 //===================================
 
-func getBuilder(builderType builderType_uint8) Builder_interface {
-	if builderType == _NORMAL_BUILDER {
+func getBuilder(this_builderType builderType_uint8) Builder_interface {
+	if this_builderType == _NORMAL_BUILDER {
 		return newNormalBuilder()
 	}
-	if builderType == _IGLOO_BUILDER {
+	if this_builderType == _IGLOO_BUILDER {
 		return newIglooBuilder()
 	}
 	return nil
@@ -147,14 +148,14 @@ func main() {
 	normalBuilder := getBuilder(_NORMAL_BUILDER)
 	iglooBuilder := getBuilder(_IGLOO_BUILDER)
 
-	director := newDirector(normalBuilder)
+	director := new_Director(normalBuilder)
 	normalHouse := director.buildHouse()
 
 	fmt.Printf("Normal House Door Type: %s\n", normalHouse.door_string)
 	fmt.Printf("Normal House Window Type: %s\n", normalHouse.window_string)
 	fmt.Printf("Normal House Num Floor: %d\n", normalHouse.floor_uint8)
 
-	director.setBuilder(iglooBuilder)
+	director.set_Builder(iglooBuilder)
 	iglooHouse := director.buildHouse()
 
 	fmt.Printf("\nIgloo House Door Type: %s\n", iglooHouse.door_string)
