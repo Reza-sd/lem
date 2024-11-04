ad0346e380b40eb5a48c1416eb77aa65ead5c809
Reza-sd <netart2000@gmail.com>
2024-11-04 13:28:47 +0000

    .

--- pkg/logstack/loggerT-Helper-Assert.go
+++ pkg/logstack/loggerT-Helper-Assert.go
@@ -6,6 +6,20 @@ import (
 	"testing"
 )
 
+//--------------------
+/*
+	%s
+
+This is a verb that formats the first argument as a string.
+It's commonly used to print plain text strings.
+%+v
+
+This is a verb with a flag +.
+The %v part formats the second argument in its default format.
+The + flag adds additional information, typically:
+For structs: It includes field names along with their values.
+For other types: It might provide more detailed output, such as pointers or addresses.
+*/
 // -------------------
 func (f *helperFn[T]) Assert(t testing.TB, got, exp any, inputsArr ...any) {
 
