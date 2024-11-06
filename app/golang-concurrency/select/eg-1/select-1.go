8fe8bd6f809c06ee16d44aac55707111648d40c0
Reza-sd <netart2000@gmail.com>
2024-11-06 15:23:54 +0000

    .

--- app/golang-concurrency/select/eg-1/select-1.go
+++ app/golang-concurrency/select/eg-1/select-1.go
@@ -1,26 +1,26 @@
 package main
 
-func m(a int, b int) int {
+func m() {
 	c := make(chan int)
-	d := 2
+	//d := 2
 
 	go func() {
 
-		c <- d
+		c <- 0
 	}()
 
 	select {
 	case res := <-c:
 		println("Received result:", res)
-		return d
+
 	default:
 		println("Timed out")
-		return 0
+
 	}
 
 }
 
 func main() {
-	println(m(4, 2))
+	m()
 
 }
