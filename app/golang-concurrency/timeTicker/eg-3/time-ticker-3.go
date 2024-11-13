4525b453ebb61df4312ec3ae1ede2ab56515d2a5
Reza-sd <netart2000@gmail.com>
2024-11-13 14:52:04 +0000

    .

---
+++ app/golang-concurrency/timeTicker/eg-3/time-ticker-3.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop() // Stop the ticker when done to free resources
+
+	done := make(chan bool)
+	go func() {
+		time.Sleep(5 * time.Second)
+		done <- true
+	}()
+
+	for {
+		select {
+		case <-done:
+			fmt.Println("Done!")
+			return
+		case t := <-ticker.C:
+			fmt.Println("Tick at", t)
+		}
+	}
+}
