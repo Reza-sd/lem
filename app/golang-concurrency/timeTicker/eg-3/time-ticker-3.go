bc4381259e3375319e1b70cfad49305241b7c95a
Reza-sd <netart2000@gmail.com>
2024-11-13 14:53:37 +0000

    .

--- app/golang-concurrency/timeTicker/eg-3/time-ticker-3.go
+++ app/golang-concurrency/timeTicker/eg-3/time-ticker-3.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop() // Stop the ticker when done to free resources
+	//Always call ticker.Stop() when you’re done with a ticker. This stops the ticker from running in the background and consuming resources.
 
 	done := make(chan bool)
 	go func() {
@@ -15,6 +16,7 @@ func main() {
 		done <- true
 	}()
 
+	//loop: This loop listens on multiple channels. When the done channel is signaled, the loop exits. Otherwise, on every tick, it prints the current time.
 	for {
 		select {
 		case <-done:
