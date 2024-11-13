608bbe833b80197ed5e8ef877bb27d786b30c9d2
Reza-sd <netart2000@gmail.com>
2024-11-13 16:25:58 +0000

    .

---
+++ app/golang-concurrency/timeTicker/eg-7/ticker-reset.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//===============================
+func doSomething(t *time.Ticker) {
+	for range t.C {
+		fmt.Println("Ticking...", time.Now())
+	}
+}
+
+//===============================
+func main() {
+	fmt.Println("Start App: ", time.Now())
+	ticker := time.NewTicker(time.Second * 2) // Initial 2-second interval
+	defer ticker.Stop()
+
+	go doSomething(ticker)
+
+	time.Sleep(7 * time.Second) // Wait for 5 seconds
+	fmt.Println("Wait for 5 seconds...")
+
+	ticker.Reset(time.Second * 1) // Reset the interval to 1 seconds
+	time.Sleep(5 * time.Second)
+	fmt.Println("END.")
+
+}
+
+//===============================
